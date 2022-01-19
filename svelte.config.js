d0c53562895513f4adbe12f7807bf91b23b50ecb
chiuzon <89428507+chiuzon@users.noreply.github.com>
2022-01-19 20:12:16 +0200

    urlq

--- svelte.config.js
+++ svelte.config.js
@@ -9,9 +9,12 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			optimizeDeps: {
+				exclude: ['@urlq/svelte']
+			}
+		}
 	}
 };
 
