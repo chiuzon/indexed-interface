c10ed22333d8153a3848619152668d692aaca40d
chiuzon <89428507+chiuzon@users.noreply.github.com>
2022-01-20 18:38:43 +0200

    index pools icons

--- src/lib/constants/indexPools.ts
+++ src/lib/constants/indexPools.ts
@@ -3,21 +3,21 @@ import { NetworksEnum, IIndexPools } from './types';
 export const INDEX_POOLS: IIndexPools = {
 	[NetworksEnum.ETHEREUM]: {
 		'degen-index': {
-			icon: '',
+			icon: 'https://indexed.finance/static/media/degen.262b6e05.png',
 			name: 'Degen Index',
 			description:
 				'A higher risk/reward index of promising Ethereum protocols that are judged as having significant room to grow.',
 			address: '0x126c121f99e1e211df2e5f8de2d96fa36647c855'
 		},
 		'nft-platform-index': {
-			icon: '',
+			icon: 'https://indexed.finance/static/media/nftp.5aa8d8be.png',
 			name: 'NFT Platform Index',
 			description:
 				'A collectors index of governance and protocol tokens drawn from both the NFT space and the wider Metaverse.',
 			address: '0x68bB81B3F67f7AAb5fd1390ECB0B8e1a806F2465'
 		},
 		'oracle-top-5-tokens-index': {
-			icon: '',
+			icon: 'https://indexed.finance/static/media/orcl5.8f5965ee.png',
 			name: 'Oracle Top 5 Tokens Index',
 			description:
 				'An index representing the current market leaders in protocols designed to bring external/real-world data onto the blockchain.',
