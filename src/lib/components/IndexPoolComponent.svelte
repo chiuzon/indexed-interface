653d26ca0407753e3ce67a9994d605d52939847c
chiuzon <89428507+chiuzon@users.noreply.github.com>
2022-01-20 22:17:03 +0200

    panel styling

--- src/lib/components/IndexPoolComponent.svelte
+++ src/lib/components/IndexPoolComponent.svelte
@@ -8,16 +8,17 @@
     })[1][id]
 
     $: poolName = indexPool.name || ''
+    $: poolDescription = indexPool.description || ''
 </script>
 
-<div class="flex flex-col border-neutral-700 border-2 rounded-t-md shadow-black hover:shadow-md hover:border-green-500 w-1/4">
+<div class="panel">
     <div class="p-2 flex justify-between">
         <div class="text-xl font-bold">{poolName}</div>
         <div class="text-xl font-semibold text-neutral-500">4$</div>
     </div>
 
-    <div class="px-3 py-2 border-l-2 text-sm font-semibold border-blue-400 mx-1 my-1">
-        A higher risk/reward index of promising Ethereum protocols that are judged as having significant room to grow.
+    <div class="px-3 py-2 border-l-2 text-sm font-semibold border-blue-400 mx-1 my-1 break-words">
+    {poolDescription}
     </div>
 
     <div class="flex flex-col justify-center items-center font-semibold border-t border-neutral-600 p-1">
